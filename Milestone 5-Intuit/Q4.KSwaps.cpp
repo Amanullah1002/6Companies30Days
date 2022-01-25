b0e94cb8cc4adab1ca60eb4a3029f4f669a7c51a
Amanullah1002 <amanullahkhan1002@gmail.com>
2022-01-25 23:20:48 +0530

    Files Added

---
+++ Milestone 5-Intuit/Q4.KSwaps.cpp
@@ -0,0 +1,50 @@
+/*
+Submitted by Amanullah Khan
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+public:
+  // Function to find the largest number after k swaps.
+  string findMaximumNum(string &str, int k)
+  {
+    // code here.
+    int size = str.length();
+    if (k <= 0)
+      return str;
+    string ans = str;
+    for (int i = 0; i < size; i++)
+    {
+      for (int j = i + 1; j < size; j++)
+      {
+        if (str[i] >= str[j])
+          continue;
+        swap(str[i], str[j]);
+        string temp = findMaximumNum(str, k - 1);
+        swap(str[i], str[j]);
+        ans = max(ans, temp);
+      }
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  int t, k;
+  string str;
+
+  cin >> t;
+  while (t--)
+  {
+    cin >> k >> str;
+    Solution ob;
+    cout << ob.findMaximumNum(str, k) << endl;
+  }
+  return 0;
+}
